d47bfd11ce5d9a95c535389ef400eba5bd5c78f0
Dmitriy Mikitenko <>
2024-12-06 15:21:52 +0200

    Add  MultiverseX token with correct info

--- Sources/MarketKit/Classes/Syncers/CoinSyncer.swift
+++ Sources/MarketKit/Classes/Syncers/CoinSyncer.swift
@@ -8,7 +8,7 @@ class CoinSyncer {
     private let keyTokensLastSyncTimestamp = "coin-syncer-tokens-last-sync-timestamp"
     private let keyInitialSyncVersion = "coin-syncer-initial-sync-version"
     private let limit = 1000
-    private let currentVersion = 4
+    private let currentVersion = 5
 
     private let storage: CoinStorage
     private let hsProvider: HsProvider
@@ -53,8 +53,9 @@ class CoinSyncer {
         let xdcToken = TokenRecord(coinUid: "xdce-crowd-sale", blockchainUid: "xdc-network", type: "native", decimals: 18)
         let mewToken = TokenRecord(coinUid: "cat-in-a-dogs-world", blockchainUid: "solana", type: "spl", decimals: 5, reference: "MEW1gQWJ3nEXg2qgERiKu7FAFj79PHvQVREQUzScPP5")
         let philToken = TokenRecord(coinUid: "philtoken", blockchainUid: "ethereum", type: "eip20", decimals: 18, reference: "0xc328a59E7321747aEBBc49FD28d1b32C1af8d3b2")
+        let elrondToken = TokenRecord(coinUid: "elrond-erd-2", blockchainUid: "elrond-erd-2", type: "native", decimals: 18)
         
-        tokenRecords.append(contentsOf: [xdcToken, mewToken, philToken])
+        tokenRecords.append(contentsOf: [xdcToken, mewToken, philToken, elrondToken])
         
         return tokenRecords
     }
